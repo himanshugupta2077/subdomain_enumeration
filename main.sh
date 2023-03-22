94dff94fb0eda9d1a474aa819b7be64481d6e62e
“zexceed012” <zexceed020@protonmail.com>
2023-03-22 22:30:41 +0530

    Each tool run 5 times to increase accuracy.

--- main.sh
+++ main.sh
@@ -12,17 +12,26 @@ if [ ! -d "$subdomain_path" ];then
 	mkdir $subdomain_path
 fi
 
-echo -e "[+] Checking in subscraper"
+echo -e "[+] Checking in subscraper ..."
 subscraper -r $subdomain_path/subscraper_output1 $domain > /dev/null && cat $subdomain_path/subscraper_output1 > $subdomain_path/found.txt
 subscraper -r $subdomain_path/subscraper_output2 $domain > /dev/null && cat $subdomain_path/subscraper_output2 >> $subdomain_path/found.txt
 subscraper -r $subdomain_path/subscraper_output3 $domain > /dev/null && cat $subdomain_path/subscraper_output3 >> $subdomain_path/found.txt
+subscraper -r $subdomain_path/subscraper_output4 $domain > /dev/null && cat $subdomain_path/subscraper_output4 >> $subdomain_path/found.txt
+subscraper -r $subdomain_path/subscraper_output5 $domain > /dev/null && cat $subdomain_path/subscraper_output5 >> $subdomain_path/found.txt
 
-echo -e "\n[+] Checking in subfinder"
+echo -e "\n[+] Checking in subfinder ..."
 subfinder -d $domain  > /dev/null 2>&1 > $subdomain_path/subfinder_output1 && cat $subdomain_path/subfinder_output1 >> $subdomain_path/found.txt
 subfinder -d $domain  > /dev/null 2>&1 > $subdomain_path/subfinder_output2 && cat $subdomain_path/subfinder_output2 >> $subdomain_path/found.txt
+subfinder -d $domain  > /dev/null 2>&1 > $subdomain_path/subfinder_output3 && cat $subdomain_path/subfinder_output3 >> $subdomain_path/found.txt
+subfinder -d $domain  > /dev/null 2>&1 > $subdomain_path/subfinder_output4 && cat $subdomain_path/subfinder_output4 >> $subdomain_path/found.txt
+subfinder -d $domain  > /dev/null 2>&1 > $subdomain_path/subfinder_output5 && cat $subdomain_path/subfinder_output5 >> $subdomain_path/found.txt
 
-echo -e "\n[+] Checking in assetfinder"
-assetfinder $domain | grep $domain > $subdomain_path/assetfinder_output && cat $subdomain_path/assetfinder_output >> $subdomain_path/found.txt
+echo -e "\n[+] Checking in assetfinder ..."
+assetfinder $domain | grep $domain > $subdomain_path/assetfinder_output1 && cat $subdomain_path/assetfinder_output1 >> $subdomain_path/found.txt
+assetfinder $domain | grep $domain > $subdomain_path/assetfinder_output2 && cat $subdomain_path/assetfinder_output2 >> $subdomain_path/found.txt
+assetfinder $domain | grep $domain > $subdomain_path/assetfinder_output3 && cat $subdomain_path/assetfinder_output3 >> $subdomain_path/found.txt
+assetfinder $domain | grep $domain > $subdomain_path/assetfinder_output4 && cat $subdomain_path/assetfinder_output4 >> $subdomain_path/found.txt
+assetfinder $domain | grep $domain > $subdomain_path/assetfinder_output5 && cat $subdomain_path/assetfinder_output5 >> $subdomain_path/found.txt
 
 cat $subdomain_path/found.txt | sort | sort -u > $subdomain_path/sorted.txt
 echo -e "\n[+] Total Subdomain Count: "
